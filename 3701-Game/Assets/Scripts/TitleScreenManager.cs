60823916abb85a90c470612b46ae7e3822923c41
Lusa <jasminely428@gmail.com>
2026-02-04 19:30:58 -0500

    Light Animation and Adjusting Title Script

--- 3701-Game/Assets/Scripts/TitleScreenManager.cs
+++ 3701-Game/Assets/Scripts/TitleScreenManager.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using static UnityEngine.Rendering.DebugUI;
 
 public class TitleScreenController : MonoBehaviour
 {
@@ -7,14 +8,21 @@ public class TitleScreenController : MonoBehaviour
     public Camera mainCamera;
     public float bottomPosition = 0f;
     public float topPosition = 10f;
-    public float panSpeed = 2f;
+    const float panSpeed = 0.01f;
+    public float currPanSpeed;
+    
 
     [Header("UI References")]
     public GameObject clickToStartUI;
     public GameObject mainMenuUI;
 
-    private bool isPanning = false;
-    private bool hasPanned = false;
+  
+
+    private enum CameraState { AtBottom, PanningUp, AtTop, PanningDown }
+    private CameraState currentState;
+
+  
+  
 
     void Start()
     {
@@ -24,39 +32,116 @@ public class TitleScreenController : MonoBehaviour
         }
 
         mainCamera.transform.position = new Vector3(0f, bottomPosition, -10f);
-        clickToStartUI.SetActive(true);
-        mainMenuUI.SetActive(false);
+        currentState = CameraState.AtBottom;
+        UpdateUIState();
+        ResetPanSpeed();
+        
     }
 
     void Update()
     {
-        if (!hasPanned && Input.GetMouseButtonDown(0))
-        {
-            isPanning = true;
-            clickToStartUI.SetActive(false);
-        }
+        HandleInput();
+        UpdateCameraPosition();
 
-        if (isPanning)
-        {
-            Debug.Log("MOOOOOOOVIIIING");
-            float currentY = mainCamera.transform.position.y;
-            float newY = Mathf.MoveTowards(currentY, topPosition, panSpeed * Time.deltaTime);
-            mainCamera.transform.position = new Vector3(0f, newY, -10f);
-
-            if (Mathf.Approximately(newY, topPosition))
-            {
-                isPanning = false;
-                hasPanned = true;
-                mainMenuUI.SetActive(true);
-            }
-        }
     }
 
+    public void HandleInput()
+    {
+        if (currentState == CameraState.AtBottom && Input.GetMouseButtonDown(0))
+        {
+            currentState = CameraState.PanningUp;
+            UpdateUIState();
+
+        }
+        else if (currentState == CameraState.AtTop && Input.GetKeyDown(KeyCode.Escape))
+        {
+            currentState = CameraState.PanningDown;
+            UpdateUIState();
+
+        }
+
+      
+    }
+    public void UpdateCameraPosition()
+    {
+
+       
+
+        if (currentState == CameraState.PanningUp)
+        {
+
+          
+            float newY = Pan(mainCamera.transform.position.y, topPosition);
+
+            mainCamera.transform.position = new Vector3(0f, newY, -10f);
+
+                if (Mathf.Approximately(newY, topPosition))
+                {
+                     currentState = CameraState.AtTop;
+                    UpdateUIState();
+                 
+
+               }
+            }
+            else if (currentState == CameraState.PanningDown)
+            {
+
+          
+            float newY = Pan(mainCamera.transform.position.y, bottomPosition);
+
+            mainCamera.transform.position = new Vector3(0, newY, -10f);
+
+                if (Mathf.Approximately(newY, bottomPosition))
+                {
+                    currentState = CameraState.AtBottom;
+                    UpdateUIState();
+               
+
+
+                }
+            }
+
+
+
+
+
+        }
+    
+
+    public void UpdateUIState()
+    {
+        clickToStartUI.SetActive(currentState == CameraState.AtBottom);
+        mainMenuUI.SetActive(currentState == CameraState.AtTop);
+    }
+
+    public float Pan(float currentY, float targetPos)
+    {
+        if (currentState == CameraState.PanningUp) RampPanSpeed();
+        if (currentState == CameraState.PanningDown) RampDownSpeed();
+
+        currPanSpeed = Mathf.Clamp(currPanSpeed, 0f, 100f); //high value is arbitrary I just need it not to go to negatives
+        float newY = Mathf.MoveTowards(currentY, targetPos, currPanSpeed * Time.deltaTime);
+        return newY;
+    }
     public void PlayGame()
     {
         SceneManager.LoadScene("Tutorial"); // Replace with name of game scene
     }
 
+    public void RampPanSpeed()
+    {
+        currPanSpeed += 0.25f; //mimic the SmoothDampen function cause idk why it's not working here
+    }
+
+    public void RampDownSpeed()
+    {
+        currPanSpeed -= 0.25f;
+    }
+
+    public void ResetPanSpeed()
+    {
+        currPanSpeed = panSpeed;
+    }
     public void QuitGame()
     {
         Application.Quit();
