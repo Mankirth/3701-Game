6848849bc5a03d67d020164e9197f58430b5027b
Mankirth <159180334+Mankirth@users.noreply.github.com>
2026-02-21 13:50:45 -0500

    Functional with placeholders

---
+++ 3701-Game/Assets/Scripts/HubNavigation.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using Unity.VisualScripting;
+using UnityEditorInternal;
+using UnityEngine;
+
+public class HubNavigation : MonoBehaviour
+{
+    [SerializeField]
+    private Transform transitionHolder, offScreenHolder, currentRoom;
+    [SerializeField]
+    private Animator animatior;
+    private bool interactable = true;
+    [SerializeField]
+    private float transitionTime = 4;
+
+    public void GoToRoom(Transform room)
+    {
+        if(!interactable)
+            return;
+        if(currentRoom != room)
+        {
+            currentRoom.transform.SetParent(offScreenHolder);
+            currentRoom = room;
+            currentRoom.SetParent(transitionHolder);
+            currentRoom.SetAsLastSibling();
+        }
+        StartCoroutine(Transition("GoToRoom"));
+    }
+
+    private IEnumerator Transition(string triggerName)
+    {
+        interactable = false;
+        animatior.SetTrigger(triggerName);
+        yield return new WaitForSeconds(transitionTime);
+        animatior.ResetTrigger(triggerName);
+        interactable = true;
+    }
+
+    public void BackToSelect()
+    {
+        if(!interactable)
+            return;
+        StartCoroutine(Transition("BackToSelect"));
+    }
+}
