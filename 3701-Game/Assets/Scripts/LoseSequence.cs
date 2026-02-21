34e3c4483f2aa4698650bc50f93719d1e7ec7aa5
Mankirth <159180334+Mankirth@users.noreply.github.com>
2026-02-21 10:52:42 -0500

    Blurry Grayscale

--- 3701-Game/Assets/Scripts/LoseSequence.cs
+++ 3701-Game/Assets/Scripts/LoseSequence.cs
@@ -7,12 +7,16 @@ public class LoseSequence : MonoBehaviour
     private Color originalColor;
     [SerializeField]
     private SpriteRenderer sprite1, sprite2;
+    private Transform camera;
+    private Vector3 origCamPos;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void OnEnable()
     {
         originalColor = sprite1.color;
         sprite1.color = new Color(originalColor.r, originalColor.g, originalColor.b, 1);
         sprite2.color = new Color(originalColor.r, originalColor.g, originalColor.b, 1);
+        camera = Camera.main.transform;
+        origCamPos = camera.transform.position;
         StartCoroutine(Lose());
     }
 
@@ -20,16 +24,22 @@ public class LoseSequence : MonoBehaviour
     {
         GameObject.Find("Player").GetComponent<SpriteRenderer>().color = Color.black;
         GameObject.Find("Enemy").GetComponent<SpriteRenderer>().color = Color.black;
-        yield return new WaitForSecondsRealtime(0.4f);
+        for(float i = 0.4f; i > 0; i--)
+        {
+            camera.position = new Vector3(Random.Range(-i, i),Random.Range(-i, i),Random.Range(-i, i)) + origCamPos;
+            yield return new WaitForSecondsRealtime(0.1f);
+        }
+        camera.position = origCamPos;
         for(float i = colourSteps; i > 0; i--)
         {
-            Debug.Log("Lose LOOP RUNNING");
             sprite1.color = new Color(originalColor.r, originalColor.g, originalColor.b, (float)i / colourSteps);
             sprite2.color = new Color(originalColor.r, originalColor.g, originalColor.b, (float)i / colourSteps);
+
             float val = (colourSteps - i) / colourSteps;
-            Debug.Log(val);
             GameObject.Find("Enemy").GetComponent<SpriteRenderer>().color = new Color(val, val, val);
             GameObject.Find("Player").GetComponent<SpriteRenderer>().color = new Color(val, val, val);
+
+            camera.position = new Vector3(Random.Range(-0.1f, 0.1f),Random.Range(-0.1f, 0.1f),Random.Range(-0.1f, 0.1f)) + origCamPos;
             yield return new WaitForSecondsRealtime(0.1f);
         }
         gameObject.SetActive(false);
