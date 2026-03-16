751f37ad306f8e03a3aadbae77a81a0d7d5c9acb
Jas <73720805+JasmineLIE@users.noreply.github.com>
2026-03-16 11:49:15 -0400

    SFX added to comic and adjusted scene flow logic

--- 3701-Game/Assets/Scripts/Comic/PanelRowObject.cs
+++ 3701-Game/Assets/Scripts/Comic/PanelRowObject.cs
@@ -5,7 +5,9 @@ using UnityEngine.UI;
 public class PanelRowObject : MonoBehaviour
 {
 
-    [SerializeField] private Image[] coverPanel;
+    [SerializeField] private GameObject[] panels;
+   
+    public AudioClip ambience;
    
     private int panelIndex;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -15,22 +17,41 @@ public class PanelRowObject : MonoBehaviour
     }
 
     //reveal panel/dialogue text and increment index
-    public void RevealPanel()
+    public AudioClip RevealPanel()
     {
+        AudioClip currSFX = null;
+        if (HasPanelLeft()) { currSFX = GetCurrSfx(); }
 
-            coverPanel[panelIndex].color = new Color(0f, 0f, 0f, 0f); //make transaprent
-            panelIndex++;
-    
+        Image currPanel = panels[panelIndex].GetComponent<Image>();
+
+            currPanel.color = new Color(0f, 0f, 0f, 0f); //make transaprent
+          
+       panelIndex++;
+        
+        return currSFX;
     }
 
     public bool HasPanelLeft()
     {
-        return panelIndex < coverPanel.Length;  //if we have more things left to reveal, return true
+        Debug.Log ("Comparing " + panelIndex + " to " + panels.Length);
+        return panelIndex < panels.Length;  //if we have more things left to reveal, return true
     }
 
     public float GetPanelYPos()
     {
         return gameObject.transform.position.y;
+    }
+
+    public AudioClip GetAmbience()
+    {
+        return ambience;
+    }
+
+    public AudioClip GetCurrSfx()
+    {
+        Debug.Log("Fetching SFX at index " +  panelIndex);
+        ComicPanelObject currPanel = panels[panelIndex].GetComponent<ComicPanelObject>();
+        return currPanel.sfx;
     }
    
 }
