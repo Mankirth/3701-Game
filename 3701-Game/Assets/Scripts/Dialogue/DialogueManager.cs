7cad5c84e46692561b6c32cc1d98e622005bb3cd
Lusa <jasminely428@gmail.com>
2026-03-16 20:06:56 -0400

    Cache weird stuff

--- 3701-Game/Assets/Scripts/Dialogue/DialogueManager.cs
+++ 3701-Game/Assets/Scripts/Dialogue/DialogueManager.cs
@@ -306,7 +306,7 @@ public class DialogueManager : MonoBehaviour
 
         GameObject newDecision = Instantiate(decisionPrefab, dialogueBox.transform);
         newDecision.GetComponent<PlayerChoiceObject>().SetText(text1, text2, target1, target2);
-        Invoke("RenderScrollBarDown", 0.025);
+        Invoke("RenderScrollBarDown", 0.025f);
     }
 
     public void CreatePlayerDialogueObject(string text)
